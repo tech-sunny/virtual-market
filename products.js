d9f751791cf1f8012a186354cd0bc4154a85192d
Saniel1990 <saniel_sabr@outlook.com>
2023-05-15 20:23:04 -0300

    feat: all features

--- products.js
+++ products.js
@@ -88,3 +88,61 @@ const products = [
     imageDescription: "",
   },
 ];
+
+let uls = document.querySelectorAll("main.products-content > ul");
+
+let listByCategory = (tagList, productArray) => {
+    for (let i = 0; i < tagList.length; i++) {
+      listProducts(tagList[i], productArray)        
+    }    
+}
+
+let listProducts = (tagHtml, productArray) => {
+    tagHtml.innerHTML = "";
+
+    for (let i = 0; i < productArray.length; i++) {
+        const element = templateCreate(productArray[i])
+        
+        if (tagHtml.parentElement.classList.contains(productArray[i].category)) {
+          tagHtml.append(element);
+        } 
+    }
+}
+
+let templateCreate = (obj) => {
+    let tagLi = document.createElement("li");
+    let tagImg = document.createElement("img");
+    let tagMain = document.createElement("main");
+
+    let tagH1 = document.createElement("h1");
+    let tagH5 = document.createElement("h5");
+    let tagStrong = document.createElement("strong");
+
+    tagLi.className = "product";
+    tagMain.className = "product-main";
+
+    if (!obj.image) {
+      obj.image = "./img/products/no-img.svg"      
+    }
+
+    tagImg.className = "product-img";
+    tagImg.src = obj.image;
+    tagImg.alt = obj.title;
+    tagImg.title = obj.title;
+
+    tagH1.className = "product-title";
+    tagH1.innerText = obj.title;
+
+    tagH5.className = "product-category";
+    tagH5.innerText = obj.category;
+    
+    tagStrong.className = "product-price";
+    tagStrong.innerText = `R$ ${obj.price}`;
+
+    tagMain.append(tagH1, tagH5, tagStrong);
+    tagLi.append(tagImg, tagMain);
+
+    return tagLi;
+}
+
+listByCategory(uls, products)
